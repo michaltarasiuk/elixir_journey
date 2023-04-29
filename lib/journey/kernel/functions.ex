539ea3780ecc998cdeda961a5d18b887efdeac4a
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-04-29 19:49:10 +0200

    fix names of functions

--- lib/journey/kernel/functions.ex
+++ lib/journey/kernel/functions.ex
@@ -1,51 +1,51 @@
 defmodule ElixirJourney.Kernel.Functions do
   # Note that, unlike and/2, this operator accepts any expression as the first argument, not only booleans.
-  def boolean_and_operator do
+  def boolean_and_operator_example do
     # true
     Enum.empty?([]) && Enum.empty?([])
   end
 
-  def list_concatenation_operator do
+  def list_concatenation_operator_example do
     # {[1, 2, 3], 'foobar'}
     {[1] ++ [2, 3], 'foo' ++ 'bar'}
   end
 
-  def list_subtraction_operator do
+  def list_subtraction_operator_example do
     # [3, 1]
     [1, 2, 3, 2, 1] -- [1, 2, 2]
   end
 
-  def full_slice_range do
+  def full_slice_range_example do
     # "Hello world!"
     String.slice("Hello world!", ..)
   end
 
-  def range do
+  def range_example do
     # {[1, 2, 3], [3, 1]}
     {Enum.to_list(1..3), Enum.to_list(3..1//-2)}
   end
 
-  def boolean_not_operator do
+  def boolean_not_operator_example do
     # {false, true}
     {!Enum.empty?([]), !List.first([])}
   end
 
-  def binary_concatenation_operator do
+  def binary_concatenation_operator_example do
     # "foobar"
     "foo" <> "bar"
   end
 
-  def text_based_match_operator do
+  def text_based_match_operator_example do
     # {true, true, true}
     {"abcd" =~ ~r/c(d)/, "abcd" =~ "bc", "abcd" =~ ""}
   end
 
-  def pipe_operator do
+  def pipe_operator_example do
     # {[2, 4, 6]}
     {[1, [2], 3] |> List.flatten() |> Enum.map(fn x -> x * 2 end)}
   end
 
-  def boolean_or_operator do
+  def boolean_or_operator_example do
     # {false, true}
     {Enum.empty?([1]) || Enum.empty?([1]), List.first([]) || true}
   end
--- lib/journey/kernel/guards.ex
+++ lib/journey/kernel/guards.ex
@@ -1,130 +1,130 @@
 defmodule ElixirJourney.Kernel.Guards do
-  def not_equal_operator do
+  def not_equal_operator_example do
     # {true, false}
     {1 != 2, 1 != 1.0}
   end
 
-  def strict_not_equal_operator do
+  def strict_not_equal_operator_example do
     # {true, true}
     {1 !== 2, 1 !== 1.0}
   end
 
-  def equal_operator do
+  def equal_operator_example do
     # {false, true}
     {1 == 2, 1 == 1.0}
   end
 
-  def strict_equal_operator do
+  def strict_equal_operator_example do
     # {false, false}
     {1 === 2, 1 === 1.0}
   end
 
-  def elem do
+  def elem_example do
     tuple = {:foo, :bar, 3}
     elem(tuple, 1)
   end
 
-  def hd do
+  def hd_example do
     # 1
     hd([1, 2, 3, 4])
   end
 
-  def left_in_right do
+  def left_in_right_example do
     # {true, false}
     {1 in [1, 2, 3], 1 not in [1, 2, 3]}
   end
 
-  def is_atom do
+  def is_atom_example do
     # true
     is_atom(nil)
   end
 
-  def is_binary do
+  def is_binary_example do
     # true
     is_binary("foo")
   end
 
-  def is_boolean do
+  def is_boolean_example do
     # true
     is_boolean(false)
   end
 
-  def is_exception do
+  def is_exception_example do
     # true
     is_exception(%RuntimeError{})
   end
 
-  def is_float do
+  def is_float_example do
     # true
     is_float(1.5)
   end
 
-  def is_function do
+  def is_function_example do
     # {true, true}
     {is_function(fn x -> x + x end), is_function(fn x -> x * 2 end, 1)}
   end
 
-  def is_integer do
+  def is_integer_example do
     # true
     is_integer(1)
   end
 
-  def is_list do
+  def is_list_example do
     # true
     is_list([])
   end
 
-  def is_map do
+  def is_map_example do
     is_map(%{})
   end
 
-  def is_map_key do
+  def is_map_key_example do
     map = %{a: 1}
 
     # true
     is_map_key(map, :a)
   end
 
-  def is_nil do
+  def is_nil_example do
     # true
     is_nil(nil)
   end
 
-  def is_number do
+  def is_number_example do
     # true
     is_number(1)
   end
 
-  def is_tuple do
+  def is_tuple_example do
     # true
     is_tuple({})
   end
 
-  def length do
+  def length_example do
     # 3
     length([1, 2, 3])
   end
 
-  def map_size do
+  def map_size_example do
     # 1
     map_size(%{a: 1})
   end
 
-  def tuple_size do
+  def tuple_size_example do
     # 2
     tuple_size({:a, 1})
   end
 
-  def not_operator do
+  def not_operator_example do
     not false
   end
 
-  def left_or_right do
+  def left_or_right_example do
     # {false, true}
     {false or false, true or "A"}
   end
 
-  def tl do
+  def tl_example do
     # [2, 3]
     tl([1, 2, 3])
   end
