64b04152b24d25ee02c1777df1a187124500503f
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-16 22:33:16 +0200

    16.06.2023

---
+++ lib/challenges/16_05_2023.ex
@@ -0,0 +1,34 @@
+defmodule ElixirJourney.Challenges.SixteenthMay2023 do
+  # csv_to_json
+  @delimeter ","
+
+  def index_of(value, search_string) do
+    value
+    |> String.split("", trim: true)
+    |> Enum.with_index()
+    |> Enum.find_value(&(elem(&1, 0) === search_string && elem(&1, 1)))
+  end
+
+  def get_titles(input) do
+    input
+    |> String.slice(0..(index_of(input, "\n") - 1))
+    |> String.split(@delimeter)
+    |> List.to_tuple()
+  end
+
+  def csv_to_json do
+    input = "col1,col2\na,b\nc,d"
+    titles = get_titles(input)
+
+    # [%{"col1" => "a", "col2" => "b"}, %{"col1" => "c", "col2" => "d"}]
+    input
+    |> String.slice((index_of(input, "\n") + 1)..String.length(input))
+    |> String.split("\n")
+    |> Enum.map(fn values ->
+      values
+      |> String.split(@delimeter)
+      |> Enum.with_index(&{elem(titles, &2), &1})
+      |> Map.new()
+    end)
+  end
+end
