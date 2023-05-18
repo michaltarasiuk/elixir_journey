b78f193c9162cf3364416a6493c9e78c9dcb47c2
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-18 15:11:51 +0200

    17.05.2023

---
+++ lib/challenges/17_05.2023.ex
@@ -0,0 +1,23 @@
+defmodule ElixirJourney.Challenges.SeventeenthMay2023 do
+  def truth_check_collection do
+    users = [%{user: "Tinky-Winky", sex: "male"}, %{user: "Dipsy", sex: "male"}]
+
+    # true
+    Enum.all?(users, fn user -> user[:sex] end)
+  end
+
+  # common_keys
+  def map_keys_to_set(map) do
+    map
+    |> Map.keys()
+    |> MapSet.new()
+  end
+
+  def common_keys do
+    user_1 = %{name: "John", age: 21}
+    user_2 = %{name: "Adam"}
+
+    # [:name]
+    MapSet.intersection(map_keys_to_set(user_1), map_keys_to_set(user_2)) |> MapSet.to_list()
+  end
+end
