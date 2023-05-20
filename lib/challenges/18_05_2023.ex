fd697dcfb1348277ad39110a321e36e39d5f3594
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-20 17:17:04 +0200

    19.05.2023

--- lib/challenges/18_05_2023.ex
+++ lib/challenges/18_05_2023.ex
@@ -3,16 +3,17 @@ defmodule ElixirJourney.Challenges.EighteenthMay2023 do
   def map_to_query_string do
     example_map = %{page: "1", size: "2kg", key: nil}
 
-    query_string_value = example_map
-    |> Map.to_list()
-    |> Enum.filter(&(elem(&1, 1)))
-    |> Enum.map_intersperse("&", fn tuple ->
-      key = elem(tuple, 0) |> Atom.to_string()
-      value = elem(tuple, 1)
+    query_string_value =
+      example_map
+      |> Map.to_list()
+      |> Enum.filter(&elem(&1, 1))
+      |> Enum.map_intersperse("&", fn tuple ->
+        key = elem(tuple, 0) |> Atom.to_string()
+        value = elem(tuple, 1)
 
-      "#{key}=#{value}"
-    end)
-    |> Enum.join()
+        "#{key}=#{value}"
+      end)
+      |> Enum.join()
 
     # "?page=1&size=2kg"
     "?" <> query_string_value
---
+++ lib/challenges/19_05_2023.ex
@@ -0,0 +1,23 @@
+defmodule ElixirJourney.Challenges.NineteenthMay2023 do
+  # flatten_map
+  def to_flatten_map(term, _key) when is_map(term) do
+    term |> flatten_map()
+  end
+
+  def to_flatten_map(term, key) do
+    Map.new([{key, term}])
+  end
+
+  def flatten_map(map) do
+    map
+    |> Map.keys()
+    |> Enum.reduce(%{}, &Map.merge(&2, Map.get(map, &1) |> to_flatten_map(&1)))
+  end
+
+  def flatten_map_example do
+    example_map = %{a: 1, b: %{c: 3, d: 4, e: %{f: 6}}}
+
+    # %{a: 1, c: 3, d: 4, f: 6}
+    flatten_map(example_map)
+  end
+end
