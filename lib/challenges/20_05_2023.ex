13e239aa2b165ee2e5c79c150b12f2448757c251
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-21 21:17:06 +0200

    20.05.2023

---
+++ lib/challenges/20_05_2023.ex
@@ -0,0 +1,50 @@
+defmodule ElixirJourney.Challenges.TwentythMay2023 do
+  # nest
+  @link :parent_id
+
+  def find_nest_item(items, id_key, id) when is_list(items) do
+    Enum.find(items, &(Map.fetch!(&1, id_key) === id))
+  end
+
+  def nest(items, id \\ nil) when is_list(items) do
+    items
+    |> Enum.filter(&(Map.fetch!(&1, @link) === id))
+    |> Enum.map(fn item ->
+      children = nest(items, Map.get(item, :id))
+
+      Map.put(item, :children, children)
+    end)
+  end
+
+  def nest_example do
+    comments = [
+      %{id: 1, parent_id: nil},
+      %{id: 2, parent_id: 1},
+      %{id: 3, parent_id: 1},
+      %{id: 4, parent_id: 2},
+      %{id: 5, parent_id: 4}
+    ]
+
+    # [
+    #   %{
+    #     children: [
+    #       %{
+    #         children: [
+    #           %{
+    #             children: [%{children: [], id: 5, parent_id: 4}],
+    #             id: 4,
+    #             parent_id: 2
+    #           }
+    #         ],
+    #         id: 2,
+    #         parent_id: 1
+    #       },
+    #       %{children: [], id: 3, parent_id: 1}
+    #     ],
+    #     id: 1,
+    #     parent_id: nil
+    #   }
+    # ]
+    nest(comments)
+  end
+end
