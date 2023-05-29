7e5d72ca98dea6af90753e938ab3b5e6ae9ef33e
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-29 13:13:05 +0200

    26.05.2023

--- lib/challenges/2023/may/26_05_2023.ex
+++ lib/challenges/2023/may/26_05_2023.ex
@@ -1,2 +1,19 @@
-defmodule ElixirJourney.Challenges.TwentySixthMay2023 do
+defmodule ElixirJourney.Challenges.TwentySixthMay2023.ZipMapDeep do
+  defp get_key(path) when is_list(path) do
+    Enum.map(path, &Access.key(&1, %{}))
+  end
+
+  defp zip_map_deep_impl(acc, {path, value}) when is_map(acc) do
+    put_in(acc, get_key(path), value)
+  end
+
+  defp zip_map_deep_impl(props, values) when is_list(props) and is_list(values) do
+    Enum.zip([props, values])
+    |> Enum.reduce(%{}, &zip_map_deep_impl(&2, &1))
+  end
+
+  def zip_map_deep_example do
+    # %{a: %{b: %{c: 1, d: 2}}}
+    zip_map_deep_impl([[:a, :b, :c], [:a, :b, :d]], [1, 2])
+  end
 end
