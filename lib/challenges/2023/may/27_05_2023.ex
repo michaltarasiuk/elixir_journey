9f9f30252c0ea4cb32209b6801751d3c690b7539
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-29 16:43:11 +0200

    27.05.2023

--- lib/challenges/2023/may/27_05_2023.ex
+++ lib/challenges/2023/may/27_05_2023.ex
@@ -1,2 +1,17 @@
-defmodule ElixirJourney.Challenges.TwentySeventhMay2023 do
+defmodule ElixirJourney.Challenges.TwentySeventhMay2023.DefaultsDeep do
+  defp defaults_deep_impl(map, source) when is_map(map) and is_map(source) do
+    Map.merge(map, source, fn _key, v1, v2 -> defaults_deep_impl(v1, v2) end)
+  end
+
+  defp defaults_deep_impl(v1, _v2) do
+    v1
+  end
+
+  def defaults_deep_example do
+    map = %{a: %{b: 2}}
+    source = %{a: %{b: 1, c: 3}}
+
+    # %{a: %{b: 2, c: 3}}
+    defaults_deep_impl(map, source)
+  end
 end
