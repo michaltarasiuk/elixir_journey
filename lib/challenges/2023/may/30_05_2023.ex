2b991543de6a0ed735945f88c5461102cf263bf6
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-05-30 16:00:16 +0200

    30.05.2023

---
+++ lib/challenges/2023/may/30_05_2023.ex
@@ -0,0 +1,24 @@
+defmodule ElixirJourney.Challenges.ThirtiethMay2023 do
+  defmodule OverSome do
+    defp over_some_impl(value, predicates) when is_list(predicates) do
+      predicates
+      |> Enum.filter(&is_function/1)
+      |> Enum.any?(fn predicate -> predicate.(value) end)
+    end
+
+    def over_some_example_1 do
+      # true
+      over_some_impl("1", [&is_binary/1, &is_atom/1])
+    end
+
+    def over_some_example_2 do
+      # false
+      over_some_impl(nil, [&is_binary/1, &is_atom/1])
+    end
+
+    def over_some_example_3 do
+      # true
+      over_some_impl(:atom, [&is_binary/1, &is_atom/1])
+    end
+  end
+end
