bf33f57cfad9f35108b90f7ebeb02b16818f13af
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-06-27 23:25:53 +0200

    13_06_2023

--- lib/challenges/2023/june/13_06_2023.ex
+++ lib/challenges/2023/june/13_06_2023.ex
@@ -1,2 +1,10 @@
-defmodule ElixirJourney.Challenges.ThirteenthOfJune2023 do
+defmodule ElixirJourney.Challenges.ThirteenthOfJune2023.OccurrenceCount do
+  defp occurrence_count([], _value), do: 0
+  defp occurrence_count([value | tail], value), do: 1 + occurrence_count(tail, value)
+  defp occurrence_count([_head | tail], value), do: occurrence_count(tail, value)
+
+  def example do
+    # 2
+    occurrence_count([1, 2, 3, 1], 1)
+  end
 end
