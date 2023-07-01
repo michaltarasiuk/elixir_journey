f9fc82d7d46056753085edd5812dedda4716370c
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-07-02 00:03:39 +0200

    clean 19.06.2023

--- lib/challenges/2023/june/19_06_2023.ex
+++ lib/challenges/2023/june/19_06_2023.ex
@@ -2,11 +2,10 @@ defmodule ElixirJourney.Challenges.NineteenthOfJune2023.MostCommonWords do
   defp find_most_common_word(words) do
     word_counts =
       words
-      |> Enum.group_by(&Function.identity/1)
-      |> Enum.map(fn {word_name, word_list} -> {word_name, Enum.count(word_list)} end)
+      |> Enum.group_by(& &1)
+      |> Enum.map(fn {word, word_list} -> {word, Enum.count(word_list)} end)
 
-    word_counts
-    |> Enum.reduce({nil, 0}, fn word_count, acc ->
+    Enum.reduce(word_counts, hd(word_counts), fn word_count, acc ->
       if elem(word_count, 1) > elem(acc, 1) do
         word_count
       else
