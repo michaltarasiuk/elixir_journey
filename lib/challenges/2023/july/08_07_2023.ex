78e1565bd3f4e04c119a789d9285349f83ec7ad9
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-07-09 20:42:02 +0200

    08.07.2023

---
+++ lib/challenges/2023/july/08_07_2023.ex
@@ -0,0 +1,22 @@
+defmodule ElixirJourney.Challenges.EighthOfJuly2023.UniqueUsername do
+  use Agent
+
+  def start_link(_) do
+    Agent.start_link(fn -> MapSet.new() end, name: __MODULE__)
+  end
+
+  def claim(name) do
+    name_is_taken = Agent.get(__MODULE__, &MapSet.member?(&1, name))
+
+    case name_is_taken do
+      false ->
+        Agent.update(__MODULE__, &MapSet.put(&1, name))
+        {:ok, :username_claimed}
+
+      true ->
+        {:error, :username_already_taken}
+    end
+  end
+
+  def usernames, do: Agent.get(__MODULE__, & &1)
+end
